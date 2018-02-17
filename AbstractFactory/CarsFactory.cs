d3f5e6e8ea7f8682c56e146389d5d65ba8123acb
0l3shka <q0l3shka@gmail.com>
2018-02-17 12:24:16 +0700

    Test Abstract Factory

---
+++ AbstractFactory/CarsFactory.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    public interface Vehicle
+    {
+       void getVType();
+    }
+
+    public interface VehicleF
+    {
+        void getFName();
+    }
+
+    class LCar : Vehicle
+    {
+        public void getVType()
+        {
+            Console.WriteLine("It's a car.");
+        }
+    }
+
+    class Truck : Vehicle
+    {
+        public void getVType()
+        {
+            Console.WriteLine("It's a truck.");
+        }
+    }
+
+    class Bus : Vehicle
+    {
+        public void getVType()
+        {
+            Console.WriteLine("It's a bus.");
+        }
+    }
+
+    class LCarF : VehicleF
+    {
+        public void getFName()
+        {
+            Console.WriteLine("We make cars.");
+        }
+    }
+    class TruckF : VehicleF
+    {
+        public void getFName()
+        {
+            Console.WriteLine("We make trucks.");
+        }
+    }
+    class BusF : VehicleF
+    {
+        public void getFName()
+        {
+            Console.WriteLine("We make buses.");
+        }
+    }
+
+    //Factory
+    public interface VehicleFactory
+    {
+        Vehicle makeVeh();
+        VehicleF makeVehF();
+    }
+
+    class CarVF : VehicleFactory
+    {
+       public Vehicle makeVeh()
+        { return new LCar(); }
+
+       public VehicleF makeVehF()
+        { return new LCarF(); }
+
+    }
+    class TruckVF : VehicleFactory
+    {
+        public Vehicle makeVeh()
+        { return new Truck(); }
+
+        public VehicleF makeVehF()
+        { return new TruckF(); }
+    }
+
+    class BusVF : VehicleFactory
+    {
+        public Vehicle makeVeh()
+        { return new Bus(); }
+
+        public VehicleF makeVehF()
+        { return new BusF(); }
+    }
+}
--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -8,8 +8,27 @@ namespace AbstractFactory
 {
     class Program
     {
+        //21 Вариант
         static void Main(string[] args)
         {
+            CarVF newCarFactory = new CarVF();
+            Vehicle car = newCarFactory.makeVeh();
+            VehicleF Cfactory = newCarFactory.makeVehF();
+
+            car.getVType();
+            Cfactory.getFName();
+
+
+            TruckVF newTruckFactory = new TruckVF();
+            Vehicle truck = newTruckFactory.makeVeh();
+            VehicleF Tfactory = newTruckFactory.makeVehF();
+
+            truck.getVType();
+            Tfactory.getFName();
+
+
+            Console.ReadLine();
+
         }
     }
 }
